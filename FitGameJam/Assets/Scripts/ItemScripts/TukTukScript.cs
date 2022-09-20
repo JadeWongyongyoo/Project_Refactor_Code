3549949e8e8aa1a474cad0c56e5585d1b61b3ef7
JadeWongyongyoo <78548186+JadeWongyongyoo@users.noreply.github.com>
2022-09-20 20:23:45 +0700

    Refactor_MoveUp_TukTuk_BGmove_ObjectMove
    Eliminate useless comments
    and the code that doesn't display

--- FitGameJam/Assets/Scripts/ItemScripts/TukTukScript.cs
+++ FitGameJam/Assets/Scripts/ItemScripts/TukTukScript.cs
@@ -3,23 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TukTukScript : MonoBehaviour
-{
-    
-    // Start is called before the first frame update
+{    
     void Start()
     {
         Invoke("destroyTukTuk", 5.0f);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void destroyTukTuk()
     {
         Destroy(gameObject);
-    }
-    
+    }    
 }
