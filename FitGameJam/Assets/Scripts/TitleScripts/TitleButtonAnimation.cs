0cb3ef2c8b16b7a41527541fcc44ec366951c6cd
Tantchamin <78613005+Tantchamin@users.noreply.github.com>
2022-10-02 01:11:15 +0700

    UPDATE: Tilte scene add minigame button, update TitleButtonAnimation script add new button animation

--- FitGameJam/Assets/Scripts/TitleScripts/TitleButtonAnimation.cs
+++ FitGameJam/Assets/Scripts/TitleScripts/TitleButtonAnimation.cs
@@ -5,7 +5,7 @@ using DG.Tweening;
 
 public class TitleButtonAnimation : MonoBehaviour
 {
-    public GameObject PlayButton, GalleryButton, CreditsButton, ExitButton;
+    public GameObject PlayButton, GalleryButton, CreditsButton, MinigameButton, ExitButton;
     
     void Start()
     {
@@ -16,9 +16,10 @@ public class TitleButtonAnimation : MonoBehaviour
     void AnimationFromLeftToRight()
     {
         PlayButton.transform.DOMoveX(320f, 1f);
-        GalleryButton.transform.DOMoveX(350f, 1.33f);
-        CreditsButton.transform.DOMoveX(380f, 1.66f);
-        ExitButton.transform.DOMoveX(410f, 2f);
+        GalleryButton.transform.DOMoveX(350f, 1.26f);
+        CreditsButton.transform.DOMoveX(380f, 1.50f);
+        MinigameButton.transform.DOMoveX(410f, 1.75f);
+        ExitButton.transform.DOMoveX(440f, 2f);
 
     }
 
